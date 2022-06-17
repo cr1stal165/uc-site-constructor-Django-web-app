f118b62fb26e174ed10eb16a37de05a76d65535c
Sergey Silnov <loginpasword2017@gmail.com>
2022-06-17 12:51:24 +0300

    add last pages

--- constructor/views.py
+++ constructor/views.py
@@ -27,19 +27,33 @@ def take_images(request):
                    "#77CC44", "#FEFAEF",
                    "#5B7FED", "#FFFFFF"]
     color = ""
+    bg_color = ""
     template_name = ""
     if request.method == 'POST':
         img = request.FILES.get("file")
         template_name = request.POST.get('r1')
         r2 = request.POST.get('r2')
         if r2 == "color1":
-            color = "F1F1F1"
+            color = "3B2A1D"
+            bg_color = "F1F1F1"
+        elif r2 == "color2":
+            color = "A61212"
+            bg_color = "FFFFFF"
+        elif r2 == "color3":
+            color = "77CC44"
+            bg_color = "FEFAEF"
+        else:
+            color = "5B7FED"
+            bg_color = "FFFFFF"
     print(color)
-    return render(request, "take_images.html", context={'color': color, 'curr': template_name})
+    return render(request, "take_images.html", context={'color': color, 'curr': template_name, 'background_color': bg_color})
 
 
 def take_info(request):
-    return render(request, "take_info.html")
+    temp_name = ""
+    if request.method == 'POST':
+        temp_name = request.POST.get('r1')
+    return render(request, "take_info.html", context={'curr': temp_name})
 
 
 def template_uc1(request):
@@ -54,8 +68,9 @@ def template_uc2(request):
 def template_uc(request):
     template_name = request.GET.get('param')
     template_color = request.GET.get('param_color')
+    template_bg_color = request.GET.get('param_bg_color')
     print(request.GET)
-    return render(request, f'{template_name}.html', context={'color': template_color})
+    return render(request, f'{template_name}.html', context={'color': template_color, 'bg_color': template_bg_color})
 
 
 def final_page(request):
